07be710875bbaa2fe80b68cd69d256464031e025
Agustin Bernal <abersit270@g.educaand.es>
2025-02-13 22:17:45 +0100

    Regalo cele

---
+++ codigos/juego.js
@@ -0,0 +1,18 @@
+function obtenerValor(){
+    let dato = document.getElementById("intro").value;
+    switch(dato){
+        case "77071":
+            alert("Pista 2: ¿Esos vinos son de decoración?, porque están llenos y no en la cocina");
+            break;
+        case "35744":
+            alert("Pista 3: Esos lentes son caros, ¿Qué hay dentro del estuche?");
+            break;
+        case "44991":
+            alert("Pista 4: Me gustaría saber que hay dentro de las estatuas egipcias");
+            break;
+        case "74926":
+            alert("Pista 5: Busca entre los peluches que te han regalado para encontrar tu premio");
+            break;
+        
+    }
+}
